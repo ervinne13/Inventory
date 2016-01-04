0705538a68e09c89341b88d1de0c3a7d2151bc11
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2016-01-05 02:18:34 +0800

    fixed production usage not applying on produce only

--- app/Http/Controllers/Modules/Production/ProductionOrdersController.php
+++ app/Http/Controllers/Modules/Production/ProductionOrdersController.php
@@ -129,7 +129,7 @@ class ProductionOrdersController extends Controller {
                 $productionOrder->postUsage();
             } else if ($productionOrder->status == "Produced") {
                 //  requery production order so details will apply
-                $productionOrder = ProductionOrder::find($id);
+                $productionOrder = ProductionOrder::find($productionOrder->doc_no);
                 $productionOrder->postUsage();
                 $productionOrder->postOutput();
             }
