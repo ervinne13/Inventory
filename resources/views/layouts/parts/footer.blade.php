198fe5bda61c138e168a36fac8e9ab79d8bb8ccc
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2016-03-04 19:41:40 +0800

    remove name

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -3,8 +3,8 @@
         <p class="text-gray-dark">
 
             <strong>{{config('app.organization')}}</strong> <i class="m-r-1 text-gray-light"></i>
-            <span class="text-gray">&#xA9; 2017. Made with 
-                <i class="fa fa-fw fa-heart text-danger"></i> by <a href="{{config('app.author_link')}}">{{config('app.author')}}</span>
+<!--            <span class="text-gray">&#xA9; 2017. Made with 
+                <i class="fa fa-fw fa-heart text-danger"></i> by <a href="{{config('app.author_link')}}">{{config('app.author')}}</span>-->
         </p>
     </div>
 </footer>
