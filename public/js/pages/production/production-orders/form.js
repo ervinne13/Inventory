07399fb9d772d00c2ea5733160d69d7dae792d74
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2016-03-05 18:43:26 +0800

    Validate incomplete stocks

--- public/js/pages/production/production-orders/form.js
+++ public/js/pages/production/production-orders/form.js
@@ -3,6 +3,7 @@
 
 (function () {
 
+    var hasIncompleteStock;
     var outOfStockTableTemplate;
     var $detailsTable;
 
@@ -33,12 +34,20 @@
         form_utilities.validate = true;
         form_utilities.useFullDetailsData = true;
         form_utilities.initializeDefaultProcessing($('.fields-container'), $detailsTable);
+        form_utilities.postValidate = validateSave;
     }
 
     function initializeEvents() {
         $('#action-refresh-details').click(function () {
             refreshProductionCostDetails();
         });
+    }
+
+    function validateSave() {
+        let status = $('#input-status').val();
+        if (hasIncompleteStock && status != "Open") {
+            swal("Error", "Incomplete stocks", "error");
+        }
     }
 
     function initializeDetailsTable() {
@@ -180,7 +189,8 @@
 
     function showOutOfStockTable(productionCostDetails) {
 
-        if (productionCostDetails && productionCostDetails.out_of_stock.length > 0) {
+        hasIncompleteStock = productionCostDetails && productionCostDetails.out_of_stock.length > 0;
+        if (hasIncompleteStock) {
             var html = outOfStockTableTemplate(productionCostDetails);
             $('#out-of-stock-table-container').html(html);
         } else {
